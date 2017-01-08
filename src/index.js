da5a20b66d21ef21a3cfbb303129cb62630c19ed
Maarten Buwalda <maarten@maartenbuwalda.com>
2017-01-08 20:26:22 +0100

    Setup with sass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.sass';
+
+import store from './store/createStore'
+
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
+)
