db1837a4fdc7928bc8827b106b93dd41c5f43297
Maarten Buwalda <maarten@maartenbuwalda.com>
2019-05-27 20:35:19 +0200

    Add heavy shadow

--- src/constants/shadows.js
+++ src/constants/shadows.js
@@ -1,3 +1,4 @@
 export default {
-  mild: '0 1px 3px rgba(0,0,0,0.0), 0 1px 2px rgba(0,0,0,0.0)',
+  mild: '0 1px 3px rgba(0,0,0,0.1), 0 1px 2px rgba(0,0,0,0.1)',
+  heavy: '0 19px 38px rgba(0,0,0,0.2), 0 15px 12px rgba(0,0,0,0.2)',
 }
