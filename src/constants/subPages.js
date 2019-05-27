98fdb332680a0d022f69493a0d67e7055b13d0f3
Maarten Buwalda <maarten@maartenbuwalda.com>
2019-05-27 20:35:32 +0200

    Update sub pages

--- src/constants/subPages.js
+++ src/constants/subPages.js
@@ -1,18 +1,21 @@
-import Transavia from '../components/Pages/work/Transavia'
+import { Transavia } from '../components/Pages/work/Transavia'
+import { GoFort } from '../components/Pages/work/GoFort'
+import { Dinst } from '../components/Pages/work/Dinst'
 import Wakoopa from '../components/Pages/work/Wakoopa'
+
 import images from './images'
 
 export const subPages = [
   {
     pathname: '/work/fontem-ventures',
-    label: '2019 - now: Fontem Ventures',
+    label: '2019: Fontem Ventures',
     title: 'Fontem Ventures',
     image: images.books,
     content: Transavia.content,
   },
   {
     pathname: '/work/battlefield-bus-tours',
-    label: '2019 - now: Battlefield Bus Tours',
+    label: '2019: Battlefield Bus Tours',
     title: 'Battlefield Bus Tours',
     image: images.books,
     content: Transavia.content,
@@ -22,14 +25,7 @@ export const subPages = [
     label: '2018 - 2019: Transavia',
     title: 'Transavia',
     image: images.transavia,
-    content: Transavia.content,
-  },
-  {
-    pathname: '/work/wakoopa',
-    label: '2015 - 2017: Wakoopa',
-    title: 'Wakoopa',
-    image: images.wkp,
-    content: Wakoopa.content,
+    content: Transavia,
   },
   {
     pathname: '/work/stichting-herstelling',
@@ -43,13 +39,20 @@ export const subPages = [
     label: '2017: Go Fort',
     title: 'Go Fort',
     image: images.fort,
-    content: Transavia.content,
+    content: GoFort,
+  },
+  {
+    pathname: '/work/wakoopa',
+    label: '2015 - 2017: Wakoopa',
+    title: 'Wakoopa',
+    image: images.wkp,
+    content: Wakoopa.content,
   },
   {
     pathname: '/work/dinst',
     label: '2014 - 2015: Dinst',
     title: 'Dinst',
     image: images.transavia,
-    content: Transavia.content,
+    content: Dinst,
   },
 ]
